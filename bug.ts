9c617f2fba8594726e8e3be8e10d7596b1bd5c9e
BugHunter-X <askzenai@gmail.com>
2025-01-10 10:34:04 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,6 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = ["Jane Doe", "John Smith"];
+console.log(greeter(user)); //Error: Argument of type 'string[]' is not assignable to parameter of type 'string'.
